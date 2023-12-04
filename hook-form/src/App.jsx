5d145b34e74dcf6e134d95631e1d5a8cb0423838
Isac <isacojeda717@gmail.com>
2023-12-04 11:45:29 -0800

    more-forms assignment (updated from hook-form)

--- hook-form/src/App.jsx
+++ hook-form/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- hook-form/src/components/Form.jsx
+++ hook-form/src/components/Form.jsx
@@ -6,52 +6,96 @@ const Form = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
     const [cpassword, setCpassword] = useState("");  
+    const[validated,setValidation] = useState("");
 
     
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { firstname,lastname, email, password,cpassword };
-    	setFirst("");
-        setLast("");
-    	setEmail("");
-    	setPassword("");
-        setCpassword("");
+    const firstName = (e) => {
+    	setFirst(e.target.value);
+        if(e.target.value.length<1){
+            setValidation("First Name is required");
+        }else if(e.target.value.length<3){
+            setValidation("First Name must be 3 Characters or longer");
+        }else{
+            setValidation("");
+        }
+    };
+    const lastName = (e) => {
+    	setLast(e.target.value);
+        if(e.target.value.length<1){
+            setValidation("");
+        }else if(e.target.value.length<3){
+            setValidation("Last Name must be 3 Characters or longer");
+        }else{
+            setValidation("");
+        }
+    };
+    const emails = (e) => {
+    	setEmail(e.target.value);
+        if(e.target.value.length<1){
+            setValidation("");
+        }else if(e.target.value.length<2){
+            setValidation("Email must be 3 Characters or longer");
+        }else{
+            setValidation("");
+        }
+    };
+    const passwords = (e) => {
+    	setPassword(e.target.value);
+        if(e.target.value.length<1){
+            setValidation("");
+        }else if(e.target.value.length<8){
+            setValidation("Password must be at least 8 Characters");
+        }else{
+            setValidation("");
+        }
+    };
+    const cPasswords = (e) => {
+    	setCpassword(e.target.value);
+        if( e.target.value != password){
+            setValidation("Passwords must match");
+        }else{
+            setValidation("");
+        }
+
     };
     
     return (
         <>
-            <form onSubmit={ createUser }>
+            <form onSubmit={ (e) => e.preventDefault() }>
                 <div>
                     <label>First Name: </label> 
-                    <input type="text" value={firstname} onChange={ (e) => setFirst(e.target.value) } />
+                    <input type="text" value={firstname} onChange={(firstName)} />
+                    {
+                    }
                 </div>
                 <div>
                     <label>Last Name: </label> 
-                    <input type="text" value={lastname} onChange={ (e) => setLast(e.target.value) } />
+                    <input type="text" value={lastname} onChange={(lastName)} />
+
                 </div>
                 <div>
-                    <label>Email Address: </label> 
-                    <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                    <label>Email: </label> 
+                    <input type="text" value={email} onChange={(emails)} />
+
                 </div>
                 <div>
-                    <label>Password: </label>
-                    <input type="text" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                    <label>Password: </label> 
+                    <input type="text" value={password} onChange={(passwords)} />
+
+                </div>
+                <div>
+                    <label>Confirm Password: </label> 
+                    <input type="text" value={cpassword} onChange={(cPasswords)} />
+
+                </div>
+
                 <input type="submit" value="Create User" />
-                </div>
-                <div>
-                    <label>Confirm Password: </label>
-                    <input type="text" value={cpassword} onChange={ (e) => setCpassword(e.target.value) } />
-                <input type="submit" value="Create User" />
-                </div>
             </form>
-
-            <p>Your Form Data</p>
-            <p>First Name {firstname}</p>
-            <p>Last Name {lastname}</p>
-            <p>Email {email}</p>
-            <p>Password {password}</p>
-            <p>Confirm Password {cpassword}</p>
-
+                    {
+                        validated ?
+                        <p>{validated}</p>:
+                        ""
+                    }
         </>
     );
 }
