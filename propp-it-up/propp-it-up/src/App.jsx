1d24fb8973c7cbb8c4fbe5c37447fb51f983b069
Isac <isacojeda717@gmail.com>
2023-12-03 19:30:27 -0800

    Putting It Together assignment(updated prop-it-up)

--- propp-it-up/propp-it-up/src/App.jsx
+++ propp-it-up/propp-it-up/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PersonCard from './components/PersonCardComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <PersonCard firstname="Jane" lastname="Doe" age="45" haircolor="Black" ></PersonCard>
-      <PersonCard firstname="Joe" lastname="Smith"age="88"haircolor="Brown"></PersonCard>
-      <PersonCard firstname="Millard" lastname="Fillmore"age="50"haircolor="Brown"></PersonCard>
-      <PersonCard firstname="Maria" lastname="Smith"age="50"haircolor="Brown"></PersonCard>
+      <PersonCard firstname ="Jane" lastname="Doe" fixage ={45}  haircolor="Black" ></PersonCard>
+      <PersonCard firstname="Joe" lastname="Smith"fixage={88}haircolor="Brown"></PersonCard>
+      <PersonCard firstname="Millard" lastname="Fillmore"fixage={50}haircolor="Brown"></PersonCard>
+      <PersonCard firstname="Maria" lastname="Smith"fixage={50}haircolor="Brown"></PersonCard>
     </>
   )
 }
--- propp-it-up/propp-it-up/src/components/PersonCardComponent.jsx
+++ propp-it-up/propp-it-up/src/components/PersonCardComponent.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+
     const {firstname} = props;
     const {lastname} = props;
-    const {age} = props;
     const {haircolor} = props;
-
+    const{fixage} = props;
+    const[age, setAge] = useState(fixage)
     return (
         <>
             <div>
             <p>  {lastname}, {firstname}</p>
-            <p>Age :{age} </p>
+            <p>Age : {age} </p>
             <p>Hair Color: {haircolor}</p>
+            <button onClick={()=> setAge(age + 1)}>  Birthday Button for {firstname}</button>
             </div>
         </>
     );
